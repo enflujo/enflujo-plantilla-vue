09cc941ea7a0c0875afc03a91c264bdfbce2e811
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2021-09-06 18:22:41 -0500

    🐛 actualizar deps + opciones webpack dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,7 +7,7 @@ module.exports = merge(base, {
   devtool: 'inline-source-map',
   target: 'web',
   devServer: {
-    contentBase: path.join(__dirname, 'docs'),
+    static: path.join(__dirname, 'docs'),
     historyApiFallback: true
   },
   module: {
